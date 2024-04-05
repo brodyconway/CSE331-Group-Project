2e6346fe52fa6e6a10cda0b4debff5f84b439173
brodyconway <143444118+brodyconway@users.noreply.github.com>
2024-04-05 14:09:49 -0400

    Fixed syntax errors

--- Problem 2/Solution.py
+++ Problem 2/Solution.py
@@ -13,16 +13,16 @@ class Solution:
         self.info = info
 
 
-    def dfs_all_paths(graph, current_node, client, visited, path):
+    def dfs_all_paths(self, graph, current_node, client, visited, path):
         visited.append(current_node)
         path_list = []
-        for neighbor in graph[current_node]:
+        for neighbor in self.graph[current_node]:
             if neighbor not in visited:
                 new_path = path + neighbor
                 if neighbor == client:
                     path_list.append(new_path)
                 else:
-                    new_paths = dfs_all_paths(graph, neighbor, client, visited, new_path)
+                    new_paths = self.dfs_all_paths(graph, neighbor, client, visited, new_path)
                     path_list.extend(new_paths)
         visited.remove(current_node)
         return path_list
@@ -30,15 +30,14 @@ class Solution:
 
 
     def output_paths(self):
-    visited = []  
-    path = []  
-    client_paths = {}
-    for client in self.info['list_clients']:
-        client_paths[client] = dfs_all_paths(graph, isp, client, visited, path)
-
-    # NOW NEED TO FIND WHICH PATH IN CLIENT_PATHS HAS NO PENALTY. PATH WITH NO PENLTY GETS ADDED TO PATHS DICTIONARY BELOW. IF ALL PATHS HAVE A PENALTY FOR A NODE JUST ADD THE FIRST PATH AT INDEX 0 TO MAKE IT EASIER
+        path = []
+        paths = {} 
+        visited = []
+        client_paths = {}
+        for client in self.info['list_clients']:
+            client_paths[client] = self.dfs_all_paths(self, self.graph, self.isp, client, visited, path)
     
-
+    # NOW NEED TO FIND WHICH PATH IN CLIENT_PATHS HAS NO PENALTY. PATH WITH NO PENLTY GETS ADDED TO PATHS DICTIONARY BELOW. IF ALL PATHS HAVE A PENALTY FOR A NODE JUST ADD THE FIRST PATH AT INDEX 0 TO MAKE IT EASIER
 
         paths, bandwidths, priorities = {}, {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
